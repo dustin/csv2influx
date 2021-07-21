17033c68a27f1b4550d32a7338789ff235edf835
Dustin Sallings <dustin@spy.net>
2021-07-20 22:16:09 -0700

    Remove default field.
    At least one field must be specified.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import           Data.List.NonEmpty   (NonEmpty (..))
 import qualified Data.List.NonEmpty   as NE
 import           Options.Applicative  (Parser, argument, eitherReader, execParser, fullDesc, help, helper, info, long,
                                        metavar, option, progDesc, short, showDefault, some, str, strOption, value,
@@ -15,7 +14,7 @@ options = Options
   <$> strOption (long "measurement" <> short 'm' <> showDefault <> value "measurement"
                  <> help "measurement column name")
   <*> option commaParser (long "tags" <> short 't' <> value [] <> help "tags column names (comma separated)")
-  <*> option commaParser' (long "fields" <> short 'f' <> value ("field" :| []) <> help "field column names (comma separated)")
+  <*> option commaParser' (long "fields" <> short 'f' <> help "field column names (comma separated)")
   <*> strOption (long "time" <> showDefault <> value "time" <> help "timestamp column name")
   <*> some (argument str (metavar "FILES..."))
 
